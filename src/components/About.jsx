b6bf637e13600992997db5c11df62b5d0a54020d
Obende Bright <154594512+brunleobe@users.noreply.github.com>
2025-08-14 00:13:31 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,9 @@ export default function About() {
       <p>
         I am a frontend developer and computer science student with a particular interest in making things
         simple and automating daily tasks. I try to keep up with security and
-        best practices, and am always looking for new things to learn. I currently live in Lagos, Nigeria I'm currently open to intern dev. roles.
+        best practices, and I'm always looking for new things to learn. I currently live in Lagos, Nigeria I'm currently open to intern dev. roles.
       </p>
     </div>
   );
 }
+
